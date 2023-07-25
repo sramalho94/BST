dad172365b5ffab7ce5673c2ec87000ccbb19984
Stephan D Ramalho <sramalho@fordham.edu>
2023-07-25 14:20:22 -0400

    finalized depth-first recursion function

--- depth-first-recur.js
+++ depth-first-recur.js
@@ -3,4 +3,5 @@ const depthFirstValues = (root) => {
 
   const leftValues = depthFirstValues(root.left)
   const rightValues = depthFirstValues(root.right)
+  return [root.val, ...leftValues, ...rightValues]
 }
